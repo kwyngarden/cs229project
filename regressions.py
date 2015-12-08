0094e383bf38919cfa4d011598ab3bef4f58fa53
Keith Wyngarden <kwyngard@stanford.edu>
2015-12-08 15:32:32 -0800

    Push plotting code and updated regressions

--- regressions.py
+++ regressions.py
@@ -1,4 +1,4 @@
-'''TODO: docstring'''
+'''Module to run regressions on data from Python.'''
 
 import csv
 import numpy as np
@@ -10,15 +10,21 @@ LABELS_FILENAME = 'out_labels.csv'
 DATA_SPLIT_FILENAME = 'data_split_indices.csv'
 
 
-def read_features_and_labels(features_filename=FEATURES_FILENAME, labels_filename=LABELS_FILENAME, feature_selection=None):
-    with open(features_filename, 'r') as features_file:
-        raw_rows = [row for row in csv.reader(features_file)]
-        feature_names = raw_rows[0]
-        feature_rows = [[float(val) for val in row] for row in raw_rows[1:]]
-        if feature_selection:
-            with open(feature_selection, 'r') as f:
-                indices = [int(s.strip()) for s in f.read().strip().split(',')]
-            feature_rows = [[v for i, v in enumerate(row) if indices[i]] for row in feature_rows]
+def read_features_and_labels(features_filename=FEATURES_FILENAME, labels_filename=LABELS_FILENAME, feature_selection=None, use_privacy_suppressed=False):
+    if use_privacy_suppressed:
+        feature_names = None
+        with open('combinedFeatures.csv', 'r') as features_file:
+            raw_rows = [row for row in csv.reader(features_file)]
+            feature_rows = [[float(val) for val in row] for row in raw_rows]
+    else:
+        with open(features_filename, 'r') as features_file:
+            raw_rows = [row for row in csv.reader(features_file)]
+            feature_names = raw_rows[0]
+            feature_rows = [[float(val) for val in row] for row in raw_rows[1:]]
+            if feature_selection:
+                with open(feature_selection, 'r') as f:
+                    indices = [int(s.strip()) for s in f.read().strip().split(',')]
+                feature_rows = [[v for i, v in enumerate(row) if indices[i]] for row in feature_rows]
     with open(labels_filename, 'r') as labels_file:
         raw_rows = [row for row in csv.reader(labels_file)]
         label_names = raw_rows[0]
@@ -90,7 +96,7 @@ def get_svm_predictions(train, dev):
     dev_features = [features for features, labels in dev]
     all_predictions = []
     for i in xrange(len(train[0][1])):
-        model = svm.SVR(kernel='linear')
+        model = svm.SVR(C=0.00000005)
         labels = [labels[i] for features, labels in train]
         model.fit(train_features, labels)
         all_predictions.append(model.predict(dev_features))
@@ -101,7 +107,7 @@ def get_svm_predictions(train, dev):
     return predictions
 
 
-def compute_percent_errors(all_labels, all_predictions):
+def compute_percent_errors(all_labels, all_predictions, use_rmse=False):
     # TODO(Keith): try getting baseline error from selecting k random examples, to see
     # how much schools with similar labels are clustering in our feature space
     num_labels = len(all_labels[0])
@@ -109,18 +115,30 @@ def compute_percent_errors(all_labels, all_predictions):
     for labels, predictions in zip(all_labels, all_predictions):
         for i in xrange(num_labels):
             error = (predictions[i] - labels[i]) / labels[i]
+            if use_rmse:
+                error = (error * labels[i]) ** 2
             all_errors[i].append(abs(error))
-    return [100.0 * np.mean(errors) for errors in all_errors]
+    
+    def get_percent_error(errors):
+        if use_rmse:
+            return np.sqrt(np.mean(errors))
+        return 100.0 * np.mean(errors)
+
+    return [get_percent_error(errors) for errors in all_errors]
 
     
 if __name__=='__main__':
     print 'Reading features and labels...'
-    feature_names, feature_rows, label_names, label_rows = read_features_and_labels(feature_selection='critical_features_debt.csv')
+    # feature_names, feature_rows, label_names, label_rows = read_features_and_labels(feature_selection='critical_features_debt.csv')
+    feature_names, feature_rows, label_names, label_rows = read_features_and_labels(use_privacy_suppressed=True)
     train, dev, test = get_data_splits(feature_rows, label_rows)
+    # dev = test # Use test set for evaluation
+
     print '\nMaking predictions...'
-    # predictions = get_knn_predictions(train, dev, k=13, weighting='inverse_distance')
+    # predictions = get_knn_predictions(train, dev, k=6, weighting='inverse_distance')
     predictions = get_knn_predictions(train, dev, k=13, weighting='uniform')
     # predictions = get_svm_predictions(train, dev)
+    
     print '\nComputing errors...'
     percent_errors = compute_percent_errors([labels for features, labels in dev], predictions)
     for i in xrange(len(label_names)):
